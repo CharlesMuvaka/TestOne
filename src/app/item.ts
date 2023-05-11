1e51c8b85f696978767c6952fc512fde92287c2d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-05-11 17:27:48 +0300

    define order blue print

--- src/app/item.ts
+++ src/app/item.ts
@@ -1,3 +1,8 @@
 export class Item {
-  constructor(public id: number, public name: String, public price: number, public category: string){}
+  constructor(public id: number, public name: string, public price: number, public category: string){}
+}
+
+export class Order{
+
+  constructor(public list: Item[], public id: number, public senderId: number){}
 }
